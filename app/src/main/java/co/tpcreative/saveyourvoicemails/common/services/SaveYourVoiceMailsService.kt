f837e7c5e7555e6c4ab7fddd937a2f93e1510dbc
thanhphong070 <187650-thanhphong070@users.noreply.gitlab.com>
2021-08-12 11:31:29 +0700

    [feature-223310032021] - Added service to app

--- app/src/main/java/co/tpcreative/saveyourvoicemails/common/services/SaveYourVoiceMailsService.kt
+++ app/src/main/java/co/tpcreative/saveyourvoicemails/common/services/SaveYourVoiceMailsService.kt
@@ -1,11 +1,55 @@
 package co.tpcreative.saveyourvoicemails.common.services
-
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
+import co.tpcreative.saveyourvoicemails.common.Utils
 
 class SaveYourVoiceMailsService : Service() {
-    override fun onBind(intent: Intent?): IBinder? {
-        TODO("Not yet implemented")
+    private val mBinder = LocalBinder() // Binder given to clients
+    override fun onBind(intent: Intent?): IBinder {
+       return  mBinder
     }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return START_NOT_STICKY
+    }
+
+    inner class LocalBinder : Binder() {
+        // Return this instance of SignalRService so clients can call public methods
+        val service: SaveYourVoiceMailsService
+            get() = this@SaveYourVoiceMailsService
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        log("Created service")
+    }
+
+    fun exitApp(){
+        stopForeground(true)
+        stopSelf()
+    }
+    fun startRecord(){
+
+    }
+    fun stopRecord(){
+
+    }
+    fun pauseRecord(){
+
+    }
+    fun resumeRecord(){
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        log("onDestroy")
+    }
+    
+}
+
+fun SaveYourVoiceMailsService.log(message: Any){
+    Utils.log(this::class.java,message)
 }
--- app/src/main/java/co/tpcreative/saveyourvoicemails/presentationlayer/home/view/HomeAct.kt
+++ app/src/main/java/co/tpcreative/saveyourvoicemails/presentationlayer/home/view/HomeAct.kt
@@ -2,10 +2,15 @@ package co.tpcreative.saveyourvoicemails.presentationlayer.home.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import co.tpcreative.saveyourvoicemails.R
+import co.tpcreative.saveyourvoicemails.common.activity.BaseActivity
+import co.tpcreative.saveyourvoicemails.common.activity.log
+import co.tpcreative.saveyourvoicemails.helper.ServiceHelper
 
-class HomeAct : AppCompatActivity() {
+class HomeAct : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        ServiceHelper.getInstance().onStartService()
+        log("HomeAct")
     }
 }
